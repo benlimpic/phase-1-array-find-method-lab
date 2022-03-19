5e704d10bb23438191cca826281d9389576e5fe6
init.defaultBranch <benlimpic@gmail.com>
2022-03-19 01:59:08 -0700

    commit tree d80ed0ac8f4026295c7f223e51e530067d2a0e91

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+
+const superbowlWin = (array1) => {
+  const winner = array1.find((array1) => array1.result === "W");
+  return winner ? winner.year : undefined;
+};
+
+superbowlWin(record);
+
+
+// //conditional ternary
+
+// condition ? exprIfTrue : exprIfFalse
+
+// //arrow
+
+//   find((element) => { /* ... */ } )
+
+// //callback
+
+// find(callbackFn)
+
+// //inline function
+
+// find(function (element) { /* ... */ })
+
+// // find an object by one of its properties
+
+// const inventory = [
+//     {name: 'apples', quantity: 2},
+//     {name: 'bananas', quantity: 0},
+//     {name: 'cherries', quantity: 5}
+//   ];
+
+//   function isCherries(fruit) {
+//     return fruit.name === 'cherries';
+//   }
+
+//   console.log(inventory.find(isCherries));
